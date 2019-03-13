0956ef3f3f71542637fc6f53e76369c8dd5efc59
morp <rutesicm27@gmail.com>
2019-03-13 20:02:53 +0100

    Add creategrid and hover function

---
+++ js/main.js
@@ -0,0 +1,37 @@
+    let boxSize= 37;
+    
+    const gridBox = document.querySelector("#container");
+    
+    let gridDivDim = 35 / boxSize;
+
+    gridBox.style.display = "grid";
+    gridBox.style.gridTemplate = `repeat(${boxSize},${gridDivDim}rem) / repeat(${boxSize},${gridDivDim}rem)`;
+    gridBox.style.justifyContent = 'center';
+    
+ 
+    
+    function createGrid(){
+        for(let i = 0; i != boxSize * boxSize; ++i) {
+        const gridDiv = document.createElement("div");
+        gridDiv.style.width = `${gridDivDim *0.998}rem`;
+        gridDiv.style.height = `${gridDivDim *0.998}rem`;
+        gridDiv.style.border = `${gridDivDim*0.002}rem solid #000000b6`;
+        gridDiv.classList.add('grid-div');
+        gridBox.appendChild(gridDiv);
+        }
+    }
+    const gridDiv = document.querySelectorAll('container');
+
+    function Hover(){
+        let gridBoxes = document.querySelectorAll("#container > div");
+        gridBoxes.forEach((div) => {
+            div.addEventListener('mouseover',function(e){
+                e.target.setAttribute('style','background: black')
+            })
+        });
+    }
+    
+
+    createGrid();
+    Hover();
+    
