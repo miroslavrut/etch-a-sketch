41f631666ac4d62ec59d6c314345320220368e4e
morp <rutesicm27@gmail.com>
2019-03-13 20:22:41 +0100

    Add mobile hover

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@
     let gridDivDim = 35 / boxSize;
 
     gridBox.style.display = "grid";
-    gridBox.style.gridTemplate = `repeat(${boxSize},${gridDivDim}rem) / repeat(${boxSize},${gridDivDim}rem)`;
+    gridBox.style.gridTemplate = `repeat(${boxSize},${gridDivDim}rem)
+     / repeat(${boxSize},${gridDivDim}rem)`;
     gridBox.style.justifyContent = 'center';
     
  
@@ -13,9 +14,9 @@
     function createGrid(){
         for(let i = 0; i != boxSize * boxSize; ++i) {
         const gridDiv = document.createElement("div");
-        gridDiv.style.width = `${gridDivDim *0.998}rem`;
-        gridDiv.style.height = `${gridDivDim *0.998}rem`;
-        gridDiv.style.border = `${gridDivDim*0.002}rem solid #000000b6`;
+        gridDiv.style.width = `${gridDivDim * 0.998}rem`;
+        gridDiv.style.height = `${gridDivDim * 0.998}rem`;
+        gridDiv.style.border = `${gridDivDim * 0.002}rem solid #000000b6`;
         gridDiv.classList.add('grid-div');
         gridBox.appendChild(gridDiv);
         }
@@ -30,6 +31,18 @@
             })
         });
     }
+
+    function Hover2(){
+        let gridBoxes = document.querySelectorAll("#container > div");
+        gridBoxes.forEach((div) => {
+            div.addEventListener('toucstart',function(e){
+                e.target.setAttribute('style','background: black')
+            })
+        });
+    }
+    const body = document.querySelector("body")
+    const clearBtn = document.createElement("button");
+    
     
 
     createGrid();
